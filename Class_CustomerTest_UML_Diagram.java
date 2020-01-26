0d32c18fab5417bef54ee259b8ea407f4a56426a
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2020-01-26 10:55:14 +0300

    Create Class_CustomerTest_UML_Diagram.java

---
+++ Class_CustomerTest_UML_Diagram.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Patrick CYUBAHIRO
+ */
+public class CustomerTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int ID = 1234;
+        String Name = "Moses";
+        char Gender = 'M';
+        
+        System.out.println("Your ID is: "+ID);
+        System.out.println("Your name is: "+Name);
+        System.out.println("Your Gender is: "+Gender);
+
+    }
+    
+}
